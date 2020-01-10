2e877920281532069a34e78cb70142fafa45f2a6
Zixing Li <zl82@cornell.edu>
2020-01-09 19:48:22 -0500

    add cookie and logout

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -12,6 +14,16 @@ mongoose.connect(keys.mongoURI);
 //   });
 
 const app = express();
+
+// app.use wires up middlewares, which comes between the app and handlers
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
+    keys: [keys.cookieKey] // to encrypt the cookie
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
