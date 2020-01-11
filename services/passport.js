7bf0ddd8b406527a1557c6e130e951062b8ad700
Zixing Li <zl82@cornell.edu>
2020-01-11 10:28:03 -0500

    refactor to use async await

--- services/passport.js
+++ services/passport.js
@@ -18,6 +18,60 @@ passport.deserializeUser((id, done) => {
   });
 });
 
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: keys.googleClientID,
+//       clientSecret: keys.googleClientSecret,
+//       callbackURL: '/auth/google/callback',
+//       proxy: true
+//     },
+
+//     (accessToken, refreshToken, profile, done) => {
+//       console.log('access token', accessToken);
+//       console.log('refresh token', refreshToken);
+//       console.log('profile', profile);
+//       User.findOne({ googleId: profile.id }) // go through User collection, find the first record in the collection with googleId=profile.id. Returns a promise.
+//         .then(existingUser => {
+//           if (existingUser) {
+//             // we have a record with the given profile id
+//             done(null, existingUser);
+//           } else {
+//             new User({ googleId: profile.id }) // creates new model instance
+//               .save()
+//               .then(user => done(null, user)); // user got back from the database
+//           }
+//         });
+//     }
+//   )
+// );
+
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: keys.googleClientID,
+//       clientSecret: keys.googleClientSecret,
+//       callbackURL: '/auth/google/callback',
+//       proxy: true
+//     },
+
+//     async (accessToken, refreshToken, profile, done) => {
+//       console.log('access token', accessToken);
+//       console.log('refresh token', refreshToken);
+//       console.log('profile', profile);
+//       const existingUser = await User.findOne({ googleId: profile.id }); // go through User collection, find the first record in the collection with googleId=profile.id. Returns a promise.
+
+//       if (existingUser) {
+//         // we have a record with the given profile id
+//         done(null, existingUser);
+//       } else {
+//         const user = await new User({ googleId: profile.id }).save(); // creates new model instance
+//         done(null, user); // user got back from the database
+//       }
+//     }
+//   )
+// );
+
 passport.use(
   new GoogleStrategy(
     {
@@ -27,21 +81,18 @@ passport.use(
       proxy: true
     },
 
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
       console.log('access token', accessToken);
       console.log('refresh token', refreshToken);
       console.log('profile', profile);
-      User.findOne({ googleId: profile.id }) // go through User collection, find the first record in the collection with googleId=profile.id. Returns a promise.
-        .then(existingUser => {
-          if (existingUser) {
-            // we have a record with the given profile id
-            done(null, existingUser);
-          } else {
-            new User({ googleId: profile.id }) // creates new model instance
-              .save()
-              .then(user => done(null, user)); // user got back from the database
-          }
-        });
+      const existingUser = await User.findOne({ googleId: profile.id }); // go through User collection, find the first record in the collection with googleId=profile.id. Returns a promise.
+
+      if (existingUser) {
+        // we have a record with the given profile id
+        return done(null, existingUser); // add "return" so we don't need "else"
+      }
+      const user = await new User({ googleId: profile.id }).save(); // creates new model instance
+      done(null, user); // user got back from the database
     }
   )
 );
